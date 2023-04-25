0c4ebed376e989a626509ef2a18060b26d83b4cb
iddrisabdulwahab <79385555+iddrismystic@users.noreply.github.com>
2023-04-25 12:29:02 +0100

    Add files via upload

---
+++ js/fun.js
@@ -0,0 +1,30 @@
+// custom css styling
+
+// width
+var width = this.style.getPropertyValue("--width");
+this.style.setProperty("--width", parseInt(width));
+
+var minWidth = this.style.getPropertyValue("--minWidth");
+this.style.setProperty("--minWidth", parseInt(minWidth));
+
+var maxWidth = this.style.getPropertyValue("--maxWidth");
+this.style.setProperty("--maxWidth", parseInt(maxWidth));
+
+// height
+var height = this.style.getPropertyValue("--height");
+this.style.setProperty("--height", parseInt(height));
+
+var minHeight = this.style.getPropertyValue("--minHeight");
+this.style.setProperty("--minHeight", parseInt(minHeight));
+
+var maxHeight = this.style.getPropertyValue("--maxHeight");
+this.style.setProperty("--maxHeight", parseInt(maxHeight));
+
+
+// margin
+var margin = this.style.getPropertyValue("--margin");
+this.style.setProperty("--margin", parseInt(margin));
+
+// padding
+var padding = this.style.getPropertyValue("--padding");
+this.style.setProperty("--padding", parseInt(padding));
